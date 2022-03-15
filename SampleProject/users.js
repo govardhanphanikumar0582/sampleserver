fb59ebe37b654721452381849e1bd2c9b17da852
govardhan <govardhanphanikumar0582@gmail.com>
2022-03-15 14:55:45 +0530

    add files

---
+++ SampleProject/users.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id : 1,
+        name : "john",
+        email : "john@gamil.com",
+    },
+    {
+        id : 2,
+        name : "Phani",
+        email : "Phani@gamil.com",
+    },
+    {
+        id : 3,
+        name : "Govardhan",
+        email : "govardhan@gamil.com",
+    },
+    {
+        id : 4,
+        name : "vijay",
+        email : "vijay@gamil.com",
+    },
+];
+
+module.exports = users;
